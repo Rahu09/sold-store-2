bddfc63b34381a51c4fcffd9ea9183a35518ea4a
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-09-09 03:36:08 +0530

    debbuging

--- apps/server/deploy.sh
+++ apps/server/deploy.sh
@@ -4,7 +4,7 @@ export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin"
 cd /home/ubuntu/sold-store-2
 git pull origin main
 yarn install
-# yarn build
+yarn build
 pm2 stop server
 pm2 start server
 # pm2 start npm --name "server" -- run "start:server"
