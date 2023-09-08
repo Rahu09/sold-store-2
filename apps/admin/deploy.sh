5f03b3a1284499d97ce3a16548129db0e077ec3a
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-09-09 01:12:19 +0530

    lint removed

--- apps/admin/deploy.sh
+++ apps/admin/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.6.0/bin"
+export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin"
 
 cd /home/ubuntu/sold-store-2
 git pull origin main
 yarn install
-yarn build
+# yarn build
 pm2 stop admin
-pm2 start npm --name "admin" -- run "start:admin"
+pm2 start admin
+# pm2 start npm --name "admin" -- run "start:admin"
--- apps/client/deploy.sh
+++ apps/client/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.6.0/bin"
+export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin"
 
 cd /home/ubuntu/sold-store-2
 git pull origin main
 yarn install
-yarn build
+# yarn build
 pm2 stop client
-pm2 start npm --name "client" -- run "start:client"
+pm2 start client
+# pm2 start npm --name "client" -- run "start:client"
